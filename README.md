b029fca63c7f0e3f96d0d99b963644248fe7c469
Song jae hyeok <wakij6587@naver.com>
2021-08-01 10:56:43 +0900

    readme fs,os,path,process 정리, 파일정리 프로젝트 완료

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Node.js 정리
 
 ## npm 시작
 
-기본적인 정보들이 저절로 입력
+기본적인 정보들이 저절로 입력          
 
     npm init --yes 
 
@@ -17,16 +17,84 @@ Node.js 정리
     npm uninstall --라이브러리이름--
 
 ## nodemon
-파일이 변경되면 자동으로 재시작한다.
-개발할 때만 필요하기 때문에 --save-dev를 뒤에 붙인다.
+파일이 변경되면 자동으로 재시작한다.          
+개발할 때만 필요하기 때문에 --save-dev를 뒤에 붙인다.          
 
     npm i nodemon --save-dev
 
-nodemon으로 app을 실행하기 위하여 package.json에
-node app이 아닌 nodemon app으로 바꿔준다.
+nodemon으로 app을 실행하기 위하여 package.json에          
+node app이 아닌 nodemon app으로 바꿔준다.          
 
     "scripts": {
         "start": "nodemon app" 
         ...
     },
+
+npm start가 아닌 nodemon app으로 시작한다.          
+
+    nodemon app
+
+## process
+노드가 동작하는 프로세스 정보를 가져온다.          
+          
+process.argv -> 시작 시 실행 경로, 인자들을 받아온다.          
+          
+process.nextTick(() => {---}) -> 현재 수행되고 있는 코드가 완료된 다음 태스크 큐에다가 넣는다.          
+          
+## os
+노드가 동작하는 운영체제 정보를 가져온다.          
+
+    const os = require('os');
+
+
+os.homedir() -> 기본 디렉토리를 받아온다.          
+
+
+## path
+파일의 경로에 접근하거나 경로에 대해 무언가를 처리할 때 사용한다.          
+
+    const path = require('path');
+
+
+path.join(os.homedir()) -> 인자로 받은 경로에 접근한다.          
+(여러 가지 인자를 전달하면 자동으로 묶어서 전달해 준다.)           
+          
+path.basename( file ) -> file의 정보만 읽어온다.           
+path.basename( file , ".js") -> file의 확장자를 제거하고 이름만 가져온다.           
+          
+path.extname( file , ".js") -> file의 확장자를 가져온다.           
+
+
+
+## fs
+사용자의 파일에 접근하고 싶을 때 사용한다.          
+
+    const fs = require('fs');
+
+
+총 세 가지 타입이 있다          
+1. fs.rename(oldPath, newPath, console.error);          
+ -> rename(예전경로, 새로운경로, 에러콜백);          
+
+
+2. fs.renameSync(oldPath, newPath);          
+ -> renameSync는 에러 콜백을 받지 않는다.          
+ 그렇기에 try catch를 이용해야 한다.          
+
+    try{
+        fs.renameSync(oldPath, newPath);
+    }catch (error){
+        consol.error(error);
+    }
+ 
+
+3. fs.promises.rename(oldPath, newPath).then().catch();            
+fs의 promises를 활용하여 rename을 한다.          
+          
+exists(파일경로) -> 해당 경로가 존재하는지 판단한다.          
+          
+mkdir(파일경로) -> 해당 경로에 파일을 만든다.          
+          
+readdir(파일경로) -> 경로에 있는 파일들을 읽어온다.          
+          
 
--- fileOrganize/app.js
+++ fileOrganize/app.js
@@ -10,6 +10,7 @@ const folder = process.argv[2];
 
 // os.homedir() - 현재 플랫폼의의 기본 디렉토리를 가르킨다..
 // path.join() - 경로를 설정하여 준다 ( 플랫폼별로 구분자가 달라진다. ) 
+// 작업을 실행 할 디렉토리를 연결한다.
 const workingDir = path.join(os.homedir(), "Pictures", folder);
 
 // 만약 인자가 없거나 해당경로에 test(인자)파일이 없으면 조건문을 실행한다.
@@ -18,5 +19,71 @@ if(!folder || !fs.existsSync(workingDir)){
     return;
 }
 
-console.log(workingDir);
+// test라는 폴더안에 video, captured, duplicated라는 폴더를 만든다.
+// join으로 파일경로에 접근
+const videoDir = path.join(workingDir, 'video'); 
+const capturedDir = path.join(workingDir, 'captured'); 
+const duplicatedDir = path.join(workingDir, 'duplicated'); 
+
+// 설정한 경로에 해당하는 파일이 없다면 파일을 만든다.
+// existsSync 파일이 있는지 체크한다.
+// mkdirSync 파일을 만든다. Sync 동기를 사용한 이유는 폴더를 만든 후 처리를 해야하기 때문이다.
+fs.existsSync(videoDir) || fs.mkdirSync(videoDir);
+fs.existsSync(capturedDir) || fs.mkdirSync(capturedDir);
+fs.existsSync(duplicatedDir) || fs.mkdirSync(duplicatedDir);
+
+const processFiles = files => {
+    files.forEach(file => {
+        if(isVideo(file)){
+            move(file, videoDir)
+        }else if(isCaptured(file)){
+            move(file, capturedDir)
+        }else if(isDuplicated(files, file)){
+            move(file, duplicatedDir)
+        } 
+    })
+}
+
+// !! 값이 있다면 true, 없다면 false
+const isVideo = file => {
+    const regExp = /(mp4|mov)$/gm;
+    const match = file.match(regExp);
+    return !!match;
+}
+const isCaptured = file => {
+    const regExp = /(png|aae)$/gm;
+    const match = file.match(regExp);
+    return !!match;
+}
+const isDuplicated = (files, file) => {
+    // IMG_ 는 duplicated 파일로 넘기고 IMG_E 는 남긴다.
+    // IMG_ 로 시작하지 않거나 IMG_E로 시작하면 조건문 실행 
+    if(!file.startsWith("IMG_") || file.startsWith("IMG_E")){
+        return false;
+    }
+
+    // split("_")를 사용하여 _를 기준으로 나눈 후 뒤 이름을 가져온다.
+    // IMG_1234 => 1234
+    const edited = `IMG_E${file.split("_")[1]}`
+    // files.find() 로 하나씩 돌며 비교한다.
+    // edited와 같으면 참을 반환한다.
+    const found = files.find(f => f.includes(edited));
+    return !!found;
+}
+
+const move = (file, newFileDir) => {
+    // 예전경로와 옮겨질 경로를 설정한다.
+    const oldPath = path.join(workingDir, file);
+    const newPath = path.join(newFileDir, file);
+    
+    fs.promises
+    .rename(oldPath, newPath) //
+    .catch(console.error);
+}
+
+// readdir 경로안에있는 파일들을 다 읽어온다. 
+fs.promises
+.readdir(workingDir) // 
+.then(processFiles)
+.catch(console.log)
 
